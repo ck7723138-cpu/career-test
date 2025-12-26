e43fbe2c71f1e9ef81185ff2260280168ce97513
ck7723138-cpu <ck7723138@gmail.com>
2025-12-26 17:14:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+let currentQuestion = 1;
+let answers = {};
+
 function startTest() {
   document.querySelector(".box").style.display = "none";
   document.getElementById("questionBox").style.display = "block";
 }
 
 function answer(choice) {
-  alert("You selected: " + choice);
+  answers["q" + currentQuestion] = choice;
+
+  if (currentQuestion === 1) {
+    // Q2 show karo
+    document.getElementById("question").innerText =
+      "Q2. How do you prefer to work?";
+    document.getElementById("btn1").innerText = "With Code / Logic";
+    document.getElementById("btn1").setAttribute("onclick", "answer('code')");
+    document.getElementById("btn2").innerText = "With Creativity";
+    document.getElementById("btn2").setAttribute("onclick", "answer('creative')");
+    currentQuestion = 2;
+  } else {
+    alert("Test completed ✅");
+    console.log(answers);
+  }
 }
