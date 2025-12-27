11f4cc8f76511d0c1aa17304cb9654602ace44bc
ck7723138-cpu <ck7723138@gmail.com>
2025-12-28 00:49:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let currentQuestion = 1;
-let answers = {};
 
 function startTest() {
   document.querySelector(".box").style.display = "none";
@@ -7,32 +6,21 @@ function startTest() {
 }
 
 function answer(choice) {
-  answers["q" + currentQuestion] = choice;
-
   if (currentQuestion === 1) {
-    // Q2 show
+    // Q2
     document.getElementById("question").innerText =
       "Q2. How do you prefer to work?";
 
     document.getElementById("btn1").innerText = "With Code / Logic";
-    document.getElementById("btn1").onclick = function () {
-      answer("code");
-    };
-
     document.getElementById("btn2").innerText = "With Creativity";
-    document.getElementById("btn2").onclick = function () {
-      answer("creative");
-    };
 
     currentQuestion = 2;
   } else {
-    // FINAL RESULT
+    // Result
     document.getElementById("question").innerText =
       "Test Completed ✅";
 
     document.getElementById("btn1").style.display = "none";
     document.getElementById("btn2").style.display = "none";
-
-    console.log(answers);
   }
 }
